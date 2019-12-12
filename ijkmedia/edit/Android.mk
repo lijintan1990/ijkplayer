9b2237203d12785a65bcb77457ff68bed3dc2137
qiubai <lijintan@qiushibaike.com>
2019-12-12 11:53:43 +0800

    暂时只能用命令来编译，现在用as跑不了了，估计只能回退到上上个版本

--- ijkmedia/edit/Android.mk
+++ ijkmedia/edit/Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := mediaEditer
-LOCAL_SRC_FILES := libs/libmediaEditer.so
+LOCAL_SRC_FILES := libs/$(APP_ABI)/libmediaEditer.so
 include $(PREBUILT_SHARED_LIBRARY)
 $(call import-module,android/cpufeatures)
